8440a0257ecd3a0fbb88f0fe2bcc0dcf315fb3bc
Aman Murari Singh <165234058+amanmurari@users.noreply.github.com>
2024-04-16 19:22:23 +0530

    Update urls.py

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   # path('admin/', admin.site.urls),
     path('', views.home,name="home"),
     path("register/",views.register,name="register"),
     path("update/<int:c_id>", views.update,name="update"),
